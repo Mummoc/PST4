e5306167629c5db1090f38e391a08e4b0e5c641d
AlexSarah <sarah.alexandre14@gmail.com>
2019-10-17 12:34:35 +0200

    Activité crée sur le bouton retour

--- app/src/main/java/com/example/pst4/AdvertActivity1.java
+++ app/src/main/java/com/example/pst4/AdvertActivity1.java
@@ -2,13 +2,33 @@ package com.example.pst4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AdvertActivity1 extends AppCompatActivity {
+
+    private Button retour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advert1);
+
+        retour = findViewById(R.id.retour);
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAdvertActivity2();
+            }
+        });
+    }
+    public void openAdvertActivity2() {
+        Intent intent = new Intent(this, AdvertActivity2.class);
+        startActivity(intent);
     }
 }
+
+
---
+++ app/src/main/java/com/example/pst4/AdvertActivity2.java
@@ -0,0 +1,15 @@
+package com.example.pst4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AdvertActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advert2);
+    }
+}
