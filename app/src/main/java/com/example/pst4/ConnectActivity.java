c5ef652a9acee130b840ea4f76013019b2536f53
Erdane <mahobah.erdane@hotmail.fr>
2019-11-04 23:06:51 +0100

    Prototype des infos sur la voiture

--- app/src/main/java/com/example/pst4/ConnectActivity.java
+++ app/src/main/java/com/example/pst4/ConnectActivity.java
@@ -9,6 +9,6 @@ public class ConnectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connect);
+        setContentView(R.layout.activity_car_info3);
     }
 }
---
+++ app/src/main/java/com/example/pst4/car_info3.java
@@ -0,0 +1,8 @@
+package com.example.pst4;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class car_info3 {
+
+}
