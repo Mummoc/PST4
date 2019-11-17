7eac1463a2e30bd8bf049bfa0569e1c521cd6325
Erdane <mahobah.erdane@hotmail.fr>
2019-11-17 21:37:21 +0100

    Design info3

--- app/src/main/java/com/example/pst4/ConnectActivity.java
+++ app/src/main/java/com/example/pst4/ConnectActivity.java
@@ -1,20 +1,28 @@
 package com.example.pst4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.graphics.Region;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ConnectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_info3);
-        Intent marque = new Intent(ConnectActivity.this, car_info2.class);
-        startActivity(marque);
+        setContentView(R.layout.activity_car_info4);
+
+        /*Mettre dans un fichier à part CAR INFO3*/
+        /*List age = new ArrayList<Integer>();
+        for (int i = 1900; i<2019; i++){
+            age.add(Integer.toString(i));
+        }
+        ArrayAdapter<Integer> spinnerArrayAdapter = new ArrayAdapter<Integer>(this, R.layout.support_simple_spinner_dropdown_item, age);
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        Spinner yearcraft = (Spinner)findViewById(R.id.spinner_yearcraft);
+        yearcraft.setAdapter(spinnerArrayAdapter);
+
+        Spinner mise_circulation = findViewById(R.id.spinner_mise_circulation);
+        mise_circulation.setAdapter(spinnerArrayAdapter);*/
     }
 }
