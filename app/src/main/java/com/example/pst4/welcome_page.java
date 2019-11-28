ba0b93c640edaa7c92006b26cf4d5f3094f674f1
AlexSarah <sarah.alexandre14@gmail.com>
2019-11-28 10:48:48 +0100

    Buttons update

--- app/src/main/java/com/example/pst4/welcome_page.java
+++ app/src/main/java/com/example/pst4/welcome_page.java
@@ -2,13 +2,17 @@ package com.example.pst4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class welcome_page extends AppCompatActivity {
 
+    private Button signin;
+    private Button createaccount;
 
     RelativeLayout rellay1, rellay2;
     Handler handler = new Handler();
@@ -29,5 +33,30 @@ public class welcome_page extends AppCompatActivity {
         rellay2 = (RelativeLayout)findViewById(R.id.rellay2);
 
         handler.postDelayed(runnable,2000);
+
+        signin = findViewById(R.id.signin);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignInActivity();
+            }
+        });
+        createaccount = findViewById(R.id.createaccount);
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openConnectActivity();
+            }
+        });
+
+    }
+
+    public void openSignInActivity() {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+    public void openConnectActivity() {
+        Intent intent = new Intent(this, ConnectActivity.class);
+        startActivity(intent);
     }
 }
