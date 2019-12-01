ff043624bddb90eb86dd82d758d1c97ff6ad31fc
Erdane <mahobah.erdane@hotmail.fr>
2019-12-01 18:44:16 +0100

    Test open camera app

--- app/src/main/java/com/example/pst4/MakePhotoActivity.java
+++ app/src/main/java/com/example/pst4/MakePhotoActivity.java
@@ -6,27 +6,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 public class MakePhotoActivity extends Activity {
     final static String DEBUG_TAG = "MakePhotoActivity";
     private static final int REQUEST_ID_IMAGE_CAPTURE = 100;
     private static final int REQUEST_ID_READ_WRITE_PERMISSION = 99;
     public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_TAKE_PHOTO = 1;
     Button makephoto;
     ImageView imageView;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,7 +47,7 @@ public class MakePhotoActivity extends Activity {
         makephoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MakePhotoActivity.this.onClick(v);
+                dispatchTakePictureIntent();
             }
         });
 
@@ -54,9 +63,41 @@ public class MakePhotoActivity extends Activity {
         }*/
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        this.startActivityForResult(intent,REQUEST_ID_IMAGE_CAPTURE);
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent((MediaStore.ACTION_IMAGE_CAPTURE));
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+            //startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            File photoFile = null;
+            try {
+                photoFile = createImagefile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this,"com.example.android.fileprovider",photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+            //startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private File createImagefile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+
     }
 
     public static List<String> checkAndRequestPermissions(Context context){
@@ -103,7 +144,15 @@ public class MakePhotoActivity extends Activity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+
+
+        /* super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode == REQUEST_ID_IMAGE_CAPTURE){
             if (resultCode == RESULT_OK){
@@ -114,7 +163,8 @@ public class MakePhotoActivity extends Activity {
             } else {
                 Toast.makeText(this, "Action failed", Toast.LENGTH_LONG).show();
             }
-        }
+        }*/
+
     }
 
    /* @Override
